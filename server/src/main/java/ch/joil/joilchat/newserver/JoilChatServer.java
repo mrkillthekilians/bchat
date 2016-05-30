f49aadc58e07dc7ac755211d55db4b130e49cf51
bananatreedad <jnt.meier@gmail.com>
2016-05-30 22:57:45 +0200

    change to new version

--- server/src/main/java/ch/joil/joilchat/newserver/JoilChatServer.java
+++ server/src/main/java/ch/joil/joilchat/newserver/JoilChatServer.java
@@ -3,7 +3,7 @@ package ch.joil.joilchat.newserver;
 /**
  * Created by bananatreedad on 23/05/16.
  */
-public class JoilCatServer {
+public class JoilChatServer {
 
     public static void main(String[] args) {
         if (args.length != 1) System.out.println("Usage: java ch.joil.joilchat.newserver.JoilChatServer <port>");
